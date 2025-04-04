83c90bcb2b540e4598f6628d5f5b4114bd33bd54
FarasatN <ferasetnovruzov1@gmail.com>
2025-04-04 16:16:35 +0400

    Initial commit

---
+++ app/src/main/java/com/farasatnovruzov/triviacracksimple/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.farasatnovruzov.triviacracksimple
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.farasatnovruzov.triviacracksimple.ui.theme.TriviaCrackSimpleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TriviaCrackSimpleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TriviaCrackSimpleTheme {
+        Greeting("Android")
+    }
+}
